cbe327ba19c5636e9ba6044c24a5ed7936c1a000
Yuri Castro <yuri.castro.dev@gmail.com>
2026-06-11 08:31:34 -0300

    chore: initialize spring boot project with kafka, jpa and postgresql dependencies

---
+++ src/main/java/com/zs/kafka_fraud_detection/KafkaFraudDetectionApplication.java
@@ -0,0 +1,13 @@
+package com.zs.kafka_fraud_detection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaFraudDetectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaFraudDetectionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zs/kafka_fraud_detection/KafkaFraudDetectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zs.kafka_fraud_detection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaFraudDetectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
